5cf18698148ded68dd3e833d8236fcf7b8484e12
Lauri Jutila <git@laurijutila.com>
2023-05-24 16:21:03 +0300

    Updates version.

--- lib/roseflow/proxycurl/version.rb
+++ lib/roseflow/proxycurl/version.rb
@@ -9,7 +9,7 @@ module Roseflow
     module VERSION
       MAJOR = 0
       MINOR = 5
-      PATCH = 1
+      PATCH = 2
       PRE = nil
 
       STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
